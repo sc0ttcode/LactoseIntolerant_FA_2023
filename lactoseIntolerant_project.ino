0b1fdd8e4d2b74c5dabfb88a59dbda128bdf75fc
sc0ttcode <149627520+sc0ttcode@users.noreply.github.com>
2023-11-04 13:42:22 -0500

    Update lactoseIntolerant_project.ino

--- lactoseIntolerant_project.ino
+++ lactoseIntolerant_project.ino
@@ -1,10 +1,44 @@
+const int buttonPin = 3;
+const int ledPin = 13;
+const int timeCyclePin = 12;
+int buttonState = 0;
+int previousButtonState = 0;
+int cheeseMoment = 0;
+
 void setup() 
 {
-  silly :3
- hi
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(timeCyclePin, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState != 0) 
+  {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    if(cheeseMoment == 0)
+    {
+      cheeseMoment++;
+      Serial.println(cheeseMoment);  
+    }
+          while(cheeseMoment == 1)
+    {
+      digitalWrite(timeCyclePin, HIGH);
+      delay(500);
+      digitalWrite(timeCyclePin, LOW);
+      delay(500);
+    }
+} 
+else 
+{
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
 }
+}
+
