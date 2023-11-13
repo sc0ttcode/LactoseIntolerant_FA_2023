4f4e9a10194658c343c18299f07a23973c606f9c
sc0ttcode <149627520+sc0ttcode@users.noreply.github.com>
2023-11-13 14:16:48 -0600

    Update lactoseIntolerant_project.ino

--- lactoseIntolerant_project.ino
+++ lactoseIntolerant_project.ino
@@ -1,44 +1,50 @@
+/* Sweep
+ by BARRAGAN http://barraganstudio.com/
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
+#include <Servo.h>
+
+Servo s1;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
 const int buttonPin = 3;
-const int ledPin = 13;
-const int timeCyclePin = 12;
-int buttonState = 0;
-int previousButtonState = 0;
-int cheeseMoment = 0;
+
+int pos = 0;    // variable to store the servo position
+int cheeseMoment = 0; //phase of action the project is in
 
 void setup() 
 {
+  s1.attach(12);  // attaches the servo on pin 9 to the servo object
   pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  pinMode(timeCyclePin, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop()
+void loop() 
 {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState != 0) 
+  if(digitalRead(buttonPin) == HIGH && cheeseMoment == 0)
   {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    if(cheeseMoment == 0)
-    {
-      cheeseMoment++;
-      Serial.println(cheeseMoment);  
-    }
-          while(cheeseMoment == 1)
-    {
-      digitalWrite(timeCyclePin, HIGH);
-      delay(500);
-      digitalWrite(timeCyclePin, LOW);
-      delay(500);
-    }
-} 
-else 
-{
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+    cheeseMoment == cheeseMoment++;
+  while(digitalRead(buttonPin) == HIGH && cheeseMoment == 1)
+  {
+    Serial.println("working");
+  for(int i = 0; i <= 180; i += 1)
+  {
+    s1.write(i);
+    delay(15);
+  }
+  for(int i = 180; i >= 0; i -= 1)
+  {
+    s1.write(i);
+    delay(15);
+  }
+  }
+  }
+    else
+  {
+    s1.write(0);
+  }
 }
-}
-
